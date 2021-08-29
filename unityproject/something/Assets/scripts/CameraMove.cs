21017a2bf05e80eddf01ce3bdb8d3260148fea03
jamer56 <66663384+jamer56@users.noreply.github.com>
2021-08-30 01:34:30 +0800

    新增FPS限制
    限制FPS 不能大於螢幕刷新率

--- unityproject/something/Assets/scripts/CameraMove.cs
+++ unityproject/something/Assets/scripts/CameraMove.cs
@@ -14,6 +14,8 @@ public class CameraMove : MonoBehaviour
     
     private void Start()
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = Screen.currentResolution.refreshRate;
         mouseSensitivity = options.dpiValue;
     }
 
